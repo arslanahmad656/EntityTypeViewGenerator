28a51f41e7bbe87c82443b9ed8662f0d2c78cee0
Arslan Ahmad <arslan.ahmad@netwrix.com>
2025-06-16 03:18:55 +0500

    test project

---
+++ ViewGenerator.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using ViewGenerator.Common;
+using ViewGenerator.Common.Models;
+using ViewGenerator.Engine;
+
+try
+{
+    var settings = new ViewEngineSettings(
+        //ConfRootFolder: @"D:\test\Conf.Demo",
+        ConfRootFolder: @"D:\test\Conf.1 (1)\Conf",
+        ConnectionString: "data source=DATASERVER;Database=388225Cx;Integrated Security=SSPI;Min Pool Size=10;MultipleActiveResultSets=True;encrypt=false",
+        IncludeForeignKeys: true,
+        ExistingViewAction: ExistingViewAction.RollbackTransaction,
+        ErrorAction: ErrorAction.Continue,
+        IncludeViews: [],
+        ExcludeViews: []
+        );
+
+    using var engine = new Generator(settings);
+    var entityTypes = await engine.GetEntityTypes(CancellationToken.None);
+    Console.WriteLine($"Total entity types: {entityTypes.Count}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
